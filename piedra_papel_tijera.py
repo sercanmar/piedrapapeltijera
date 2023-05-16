3e9e80c47b8c48de9f001d6b675994c4210be183
sercanmar <sercanmar@alu.edu.gva.es>
2023-05-16 13:24:14 +0200

    t2

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -6,6 +6,12 @@ tijera = 'tijera'
 opciones = [piedra, papel, tijera]
 gana_usuario = [[papel, piedra], [tijera, papel], [piedra, tijera]]
 gana_ordenador = [[piedra, papel], [papel, tijera], [tijera, piedra]]
+nombre = input("dime tu nombre")
+rondas = int(input("dime el numero de rondas"))
+empezar = input("Quieres jugar? (s/n): ")
+intentos = 0
+victorias_usuario = 0
+victorias_ordenador = 0
 
 
 def eleccionordenador():
@@ -23,11 +29,11 @@ def ganador(eleccion, eleccion_ordenador):
 
 print("JUEGO : Piedra, papel y tijera")
 while 1:
-    empezar = input("Quieres jugar? (s/n): ")
     if 's' in empezar.lower():
-        nombre = input("dime tu nombre")
-        eleccion_ordenador = eleccionordenador()
-        while True and 1 == 1:
+        while True:
+            eleccion_ordenador = eleccionordenador()
+            if intentos == rondas:
+                break
             eleccion_usuario = input(
                 "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras): ").lower()
             print(f"Elección del ordenador: {eleccion_ordenador}")
@@ -41,15 +47,31 @@ while 1:
                 print(f"Elección del usuario: {eleccion}")
                 if ganador(eleccion, eleccion_ordenador) == 1:
                     print(f"Gana {nombre} !!!")
+                    victorias_usuario += 1
+                    intentos += 1
                 elif ganador(eleccion, eleccion_ordenador) == -1:
                     print(f"pierde {nombre} !!!")
+                    victorias_ordenador += 1
+                    intentos += 1
                 elif ganador(eleccion, eleccion_ordenador) == 0:
                     print("Empate !!!")
-                break
+                    intentos += 1
+
             else:
                 print("Entrada incorrecta. Vuelve a intentar.")
+
     elif 'n' in empezar.lower():
         break
+    if intentos == rondas:
+        if victorias_usuario > victorias_ordenador:
+            print(f"el ganador final es {nombre} con {victorias_usuario} victorias")
+            break
+        if victorias_ordenador > victorias_usuario:
+            print(f"el ganador final es ordenador con {victorias_ordenador} victorias")
+            break
+        if victorias_ordenador == victorias_usuario:
+            print(f"Ha habido un empate con {victorias_usuario} victorias cada uno")
+            break
     else:
         print('Entrada incorrecta. Vuelve a intentar.')
     print()
