b13a691629ea9e16e0849bfa4bdd4a239597c2f9
sercanmar <sercanmar@alu.edu.gva.es>
2023-05-16 13:49:34 +0200

    v4

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -4,9 +4,12 @@ import sys
 piedra = 'piedra'
 papel = 'papel'
 tijera = 'tijera'
-opciones = [piedra, papel, tijera]
-gana_usuario = [[papel, piedra], [tijera, papel], [piedra, tijera]]
-gana_ordenador = [[piedra, papel], [papel, tijera], [tijera, piedra]]
+lagarto = 'lagarto'
+opciones = [piedra, papel, tijera, lagarto]
+gana_usuario = [[papel, piedra], [tijera, papel], [piedra, tijera], [lagarto, papel], [piedra, lagarto],
+                [tijera, lagarto]]
+gana_ordenador = [[piedra, papel], [papel, tijera], [tijera, piedra], [papel, lagarto], [lagarto, piedra],
+                  [lagarto, tijera]]
 
 
 def eleccionordenador():
@@ -22,25 +25,27 @@ def ganador(eleccion, eleccion_ordenador):
     return 0
 
 
-print("JUEGO : Piedra, papel y tijera")
+print("JUEGO : Piedra, papel, tijera y lagarto")
 while 1:
     empezar = input("Quieres jugar? (s/n): ")
     if 's' in empezar.lower():
         eleccion_ordenador = eleccionordenador()
         while True:
             eleccion_usuario = input(
-                "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras): ").lower()
-            if eleccion_usuario == "TERMINAR" or "terminar":
+                "Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras / 'l' para lagarto): ").lower()
+            print(f"Elección del ordenador: {eleccion_ordenador}")
+            if eleccion_usuario == 'terminar':
                 print("Tienes miedo?")
                 sys.exit()
-            print(f"Elección del ordenador: {eleccion_ordenador}")
-            if 'p' or 'a' or 't' or 'p' or 'a' or 't' in eleccion_usuario:
+            if 'p' or 'a' or 't' or 'l' in eleccion_usuario:
                 if 'p' in eleccion_usuario:
                     eleccion = piedra
                 elif 'a' in eleccion_usuario:
                     eleccion = papel
                 elif 't' in eleccion_usuario:
                     eleccion = tijera
+                elif 'l' in eleccion_usuario:
+                    eleccion = lagarto
                 print(f"Elección del usuario: {eleccion}")
                 if ganador(eleccion, eleccion_ordenador) == 1:
                     print("Gana el usuario !!!")
