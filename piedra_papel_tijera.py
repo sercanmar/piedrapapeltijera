a86980ffae673677cc96e126218ed879af92772f
sercanmar <sercanmar@alu.edu.gva.es>
2023-05-16 12:22:03 +0200

    v1

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -4,12 +4,14 @@ PI = 'piedra'
 PA = 'papel'
 TI = 'tijera'
 o = [PI, PA, TI]
-p = [[PA, PI],  [TI, PA],  [PI, TI]]
-n = [[PI, PA],  [PA, TI],  [TI, PI]]
+p = [[PA, PI], [TI, PA], [PI, TI]]
+n = [[PI, PA], [PA, TI], [TI, PI]]
+
 
 def gcm():
     m = random.choice(o)
     return m
+
 
 def fg(um, cm):
     if [um, cm] in p:
@@ -18,23 +20,24 @@ def fg(um, cm):
         return -1
     return 0
 
+
 print("JUEGO : Piedra, papel y tijera")
 while 1:
     c = input("Quieres jugar? (s/n): ")
-    if 's'   in c.lower():
+    if 's' in c.lower():
         cm = gcm()
-        while True and 1==1:
+        while True and 1 == 1:
             m = input("Selecciona un movimiento ('p' para piedra / 'a' para papel / 't' para tijeras): ").lower()
             print(f"Elección del ordenador: {cm}")
-            if 'p' in m  or  'a' in m  or  't' in m  or  'p' in m  or  'a' in m  or  't' in m:
-                if 'p' in m  and  'p' in m :
+            if 'p' in m or 'a' in m or 't' in m or 'p' in m or 'a' in m or 't' in m:
+                if 'p' in m and 'p' in m:
                     um = PI
-                elif 'a' in m  and  'a' in m:
+                elif 'a' in m and 'a' in m:
                     um = PA
-                elif 't' in m  and  't' in m:
+                elif 't' in m and 't' in m:
                     um = TI
                 print(f"Elección del usuario: {um}")
-                if fg(um, cm) == 1 and 1 == fg(um, cm) :
+                if fg(um, cm) == 1 and 1 == fg(um, cm):
                     print("Gana el usuario !!!")
                 elif fg(um, cm) == -1:
                     print("Gana el ordenador !!!")
@@ -52,11 +55,3 @@ while 1:
     else:
         print('Entrada incorrecta. Vuelve a intentar.')
     print()
-
-
-
-
-
-
-
-
